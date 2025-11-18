369789377444ccd518022ed829cc9e4bd6e6788b
kishor2121 <kishorkumarhkumar@gmail.com>
2025-11-18 17:58:53 +0530

    intial commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Node backend is working!");
