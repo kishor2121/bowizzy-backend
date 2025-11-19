9a607d3a66917f2c7515bbd0edef68f301568600
kishor2121 <kishorkumarhkumar@gmail.com>
2025-11-19 22:15:34 +0530

    user code fixes

--- src/app.js
+++ src/app.js
@@ -12,13 +12,13 @@ app.use(morgan("dev"));
 
 // 2. Import your routes
 const authRouter = require("./routes/auth");
-const usersRouter = require("./routes/users");
+// const usersRouter = require("./routes/users");
 const personalDetailsRouter = require("./routes/personalDetails");
 const educationRouter = require("./routes/education");
 
 // 3. Use routes under specific paths
 app.use("/auth", authRouter);
-app.use("/users", usersRouter);
+// app.use("/users", usersRouter);
 app.use("/", personalDetailsRouter);
 app.use("/", educationRouter);
 
